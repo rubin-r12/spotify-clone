6625745a87e224096626f55814d32441f9c8c589
Rubin <rubinradhakrishnan@gmail.com>
2020-08-25 16:45:16 +0530

    Add Login Page and User Authentication

---
+++ spotify-clone-app/src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from './spotify';
+
+function App() {
+  const [token, setToken] = useState(null);
+  // Runs code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('I have a token>>> ', token)
+  }, [])
+
+  return (
+    <div className="app">
+      {
+        token ? (
+          <h1>I am logged in </h1>
+        ) : (
+          <Login />
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
