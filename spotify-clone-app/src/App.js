48d1fae7750a36b44da028a7a7c45dd4e0e3b1ea
Rubin <rubinradhakrishnan@gmail.com>
2020-08-25 21:41:33 +0530

    Set Data Layer as React Context API

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
   // Runs code based on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -12,20 +19,28 @@ function App() {
     const _token = hash.access_token;
     
     if (_token) {
-      setToken(_token)
+      
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
 
-    console.log('I have a token>>> ', token)
-  }, [])
+  }, []);
 
   return (
     <div className="app">
       {
-        token ? (
-          <h1>I am logged in </h1>
-        ) : (
+        token ? 
+          <Player spotify={spotify} /> : 
           <Login />
-        )
       }
     </div>
   );
